3e6125e22868d8c091d5da9da7fb77c701a222d6
Bosco-hub <Boscomirandac@outlook.com>
2024-09-07 23:12:08 -0600

    Whole Game

--- .venv/snake.py
+++ .venv/snake.py
@@ -1,31 +1,38 @@
 from turtle import Turtle
-X_POSITION = [0,-20,-40]
+STARTING_POSITIONS = STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 
 MOVE_DISTANCE = 20
 class Snake:
     def __init__(self):
         self.snake_body = []
         self.create_snake()
+        self.head = self.snake_body[0]
 
     def create_snake(self):
         # Creates Snake
-        for t in range(3):
-            body_part = Turtle(shape="square")
-            body_part.penup()
-            body_part.color("white")
-            body_part.goto(x=X_POSITION[t], y=0)
-            self.snake_body.append(body_part)
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+    def add_segment(self, position):
+        body_part = Turtle(shape="square")
+        body_part.penup()
+        body_part.color("white")
+        body_part.goto(position)
+        self.snake_body.append(body_part)
+    def extend(self):
+        # Add a new segment ot the snake
+        self.add_segment(self.snake_body[-1].position())
+
     def move(self):
         #  Moves snake
         for seg_num in range(len(self.snake_body) - 1, 0, -1):
             new_X = self.snake_body[seg_num - 1].xcor()
             new_Y = self.snake_body[seg_num - 1].ycor()
             self.snake_body[seg_num].goto(new_X, new_Y)
-        self.snake_body[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     # To move +30 left -30 Right
     def left(self):
-        self.snake_body[0].left(90)
+        self.head.left(90)
     def right(self):
-        self.snake_body[0].right(90)
+        self.head.right(90)
 
